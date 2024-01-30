d60a149ef7931049b326c1b147270094704cb436
ADEBIYIAYOMIDE <temiayomi99@gmail.com>
2024-01-30 10:39:15 -0800

    hospital website

---
+++ index.js
@@ -0,0 +1,32 @@
+function myBars() {
+    let bars = document.querySelector("#bar");
+    let nav = document.querySelector(".navigation");
+    bars.onclick = function () {
+        if (nav.style.right == "0%") {
+            nav.style.right = "-50%";
+            bars.src = "img/menu.png"
+        } else {
+            nav.style.right = "0%";
+            bars.src = "img/x.png"
+        }
+    }
+
+}
+myBars()
+
+function toTop(){
+    let top = document.querySelector(".top");
+    window.onscroll = ()=>{
+        if(window.scrollY > 100 || document.documentElement.scrollTop > 100){
+            top.style.display = "block"
+        }else{
+            top.style.display = "none"
+        }
+    }
+
+    top.onclick = ()=>{
+        scrollTo(0,0)
+    }
+
+}
+toTop();
